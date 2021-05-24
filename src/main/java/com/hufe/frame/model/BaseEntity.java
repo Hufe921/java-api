b7affd4b18c1b47b8f1c1ceed7213cc7bfcc1b4d
Hufe921 <huangyunfeihufe@hotmail.com>
2021-05-24 21:04:22 +0800

    feat：add DynamicUpdate

--- src/main/java/com/hufe/frame/model/BaseEntity.java
+++ src/main/java/com/hufe/frame/model/BaseEntity.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.*;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -17,6 +14,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @DynamicInsert
+@DynamicUpdate
 @Where(clause = "is_active = true")
 public class BaseEntity {
 
